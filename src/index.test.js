4aa0a0468dbd3f8c1605fbb8e069512362e9119b
sarkis1231 <sakooghly@gmail.com>
2021-07-27 17:34:15 +0400

    test some mock functions

--- src/index.test.js
+++ src/index.test.js
@@ -49,5 +49,37 @@ test("this", () => {
   b.name = "b"
   const bound = myMock.bind(b);
   bound();
-  console.log(myMock.mock.instances)
+
+})
+
+
+test("someMockFunctions", () => {
+  const someMockFunction = jest.fn(() => 'return value')
+  someMockFunction('first arg', 'second arg')
+  // The function was called exactly once
+  expect(someMockFunction.mock.calls.length).toBe(1);
+
+  // The first arg of the first call to the function was 'first arg'
+  expect(someMockFunction.mock.calls[0][0]).toBe('first arg');
+
+  // The second arg of the first call to the function was 'second arg'
+  expect(someMockFunction.mock.calls[0][1]).toBe('second arg');
+
+  // The return value of the first call to the function was 'return value'
+  expect(someMockFunction.mock.results[0].value).toBe('return value');
+
+  const SomeMockConstructor = jest.fn();
+  const a = new SomeMockConstructor();
+  a.name = "test"
+  const b = new SomeMockConstructor();
+  b.name = "hello"
+
+
+  // This function was instantiated exactly twice
+  expect(SomeMockConstructor.mock.instances.length).toBe(2);
+
+  // The object returned by the first instantiation of this function
+  // had a `name` property whose value was set to 'test'
+  expect(SomeMockConstructor.mock.instances[0].name).toEqual('test');
+  expect(SomeMockConstructor.mock.instances[1].name).toEqual('hello');
 })
